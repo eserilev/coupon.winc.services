55017401d9b64bfa9fc335fc7b5643f86888282c
Eitan Seri-Levi <eserilev@ucsc.edu>
2022-01-15 23:23:02 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/eserilev/migration.winc.services
+
+go 1.17
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
-	// "github.com/clubw/content.winc.services/server"
+
+	"github.com/eserilev/migration.winc.services/server"
 )
 
 func main() {
 	fmt.Println("sandbox.winc.services")
+	server.Start()
 }
