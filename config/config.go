741979a17d2503c82cc457e19029e1ac315b2d36
Eitan Seri-Levi <eserilev@ucsc.edu>
2022-01-22 15:12:38 +0200

    update

--- config/config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-func CwApiBaseUrl() string {
-	return "http://localhost:3000"
-}
-
-func CorporateOrderRelativePath(userGuid string) string {
-	return "/__s/v2/tmp/todo/orders/corporate?userGuid=" + userGuid
-}
